da44d46bd32f229b3661c31bb93e223f06e481a0
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2025-11-23 15:08:17 +0100

    generate prio list

--- src/main.rs
+++ src/main.rs
@@ -51,20 +51,11 @@ fn write_distances_to_csv<P: AsRef<Path>>(
     Ok(())
 }
 
-fn main() -> anyhow::Result<()> {
-    const DISTANCE_CSV_PATH: &str = "./distance.csv";
-    const TEST_CASE_FOLDER: &str = "Mozilla_TCs";
-
+fn calculate_distances<P: AsRef<Path>>(
+    files: &[P],
+) -> anyhow::Result<IndexMap<(PathBuf, PathBuf), f32, foldhash::fast::RandomState>> {
     let now = Instant::now();
-    let paths = get_file_paths_from_dir(TEST_CASE_FOLDER)?;
-    let elapsed_time = now.elapsed();
-    println!(
-        "get_file_paths_from_dir took: {}",
-        elapsed_time.as_secs_f32()
-    );
-
-    let now = Instant::now();
-    let file_combos = get_file_combinations(&paths)?;
+    let file_combos = get_file_combinations(&files)?;
     let elapsed_time = now.elapsed();
     println!("get_file_combinations took: {}", elapsed_time.as_secs_f32());
 
@@ -93,9 +84,203 @@ fn main() -> anyhow::Result<()> {
         distances.insert((p1.clone(), p2.clone()), distance);
     }
     let elapsed_time = now.elapsed();
-    println!("calc distances took: {}", elapsed_time.as_secs_f32());
+    println!("distances calculation took: {}", elapsed_time.as_secs_f32());
+
+    Ok(distances)
+}
+
+fn prioritize_distances(
+    distances: &IndexMap<(PathBuf, PathBuf), f32, foldhash::fast::RandomState>,
+) -> anyhow::Result<Vec<PathBuf>> {
+    let now = Instant::now();
+    let max = distances
+        .values()
+        .cloned()
+        .enumerate()
+        .reduce(|(ia, a), (ib, b)| match a.total_cmp(&b) {
+            std::cmp::Ordering::Less => (ib, b),
+            std::cmp::Ordering::Equal => (ia, a),
+            std::cmp::Ordering::Greater => (ia, a),
+        })
+        .unwrap();
+
+    let max_kv = distances.get_index(max.0).unwrap();
+    let elapsed_time = now.elapsed();
+    println!("find biggest distance took: {}", elapsed_time.as_secs_f32());
+
+    let now = Instant::now();
+    let mut min_dist_tmp_map: HashMap<PathBuf, Option<f32>, foldhash::fast::RandomState> =
+        foldhash::HashMap::new();
+
+    let mut prio_vec: Vec<PathBuf> = vec![max_kv.0.0.to_path_buf(), max_kv.0.1.to_path_buf()];
+
+    min_dist_tmp_map.insert(max_kv.0.0.to_path_buf(), None);
+    min_dist_tmp_map.insert(max_kv.0.1.to_path_buf(), None);
+
+    loop {
+        for ((p1, p2), d) in distances {
+            match (min_dist_tmp_map.get(p1), min_dist_tmp_map.get(p2)) {
+                (Some(Some(_)), Some(Some(_))) => continue,
+                (None, None) => continue,
+                (Some(None), Some(None)) => continue,
+                (None, Some(None)) => min_dist_tmp_map.insert(p1.to_path_buf(), Some(*d)),
+                (Some(None), None) => min_dist_tmp_map.insert(p2.to_path_buf(), Some(*d)),
+                (Some(None), Some(Some(d2))) => {
+                    min_dist_tmp_map.insert(p2.to_path_buf(), Some(d2.min(*d)))
+                }
+                (Some(Some(d2)), Some(None)) => {
+                    min_dist_tmp_map.insert(p1.to_path_buf(), Some(d2.min(*d)))
+                }
+                (None, Some(Some(d2))) => {
+                    min_dist_tmp_map.insert(p2.to_path_buf(), Some(d2.min(*d)))
+                }
+                (Some(Some(d2)), None) => {
+                    min_dist_tmp_map.insert(p1.to_path_buf(), Some(d2.min(*d)))
+                }
+            };
+        }
+
+        // find max val in min dist
+        let prio = min_dist_tmp_map
+            .iter()
+            .filter(|(_, v)| v.is_some())
+            .reduce(|(pa, a), (pb, b)| {
+                match a
+                    .expect("filtered to only be some")
+                    .total_cmp(&b.expect("filtered to only be some"))
+                {
+                    std::cmp::Ordering::Less => (pb, b),
+                    std::cmp::Ordering::Equal => (pa, a),
+                    std::cmp::Ordering::Greater => (pa, a),
+                }
+            })
+            .unwrap();
+
+        prio_vec.push(prio.0.to_path_buf());
+        min_dist_tmp_map.insert(prio.0.to_path_buf(), None);
+
+        if min_dist_tmp_map.values().all(|val| val.is_none()) {
+            break;
+        }
+    }
+    let elapsed_time = now.elapsed();
+    println!(
+        "create prio distance list took: {}",
+        elapsed_time.as_secs_f32()
+    );
+
+    Ok(prio_vec)
+}
+
+fn main() -> anyhow::Result<()> {
+    const DISTANCE_CSV_PATH: &str = "./distance.csv";
+    const TEST_CASE_FOLDER: &str = "Mozilla_TCs";
+
+    let now = Instant::now();
+    let paths = get_file_paths_from_dir(TEST_CASE_FOLDER)?;
+    let elapsed_time = now.elapsed();
+    println!(
+        "get_file_paths_from_dir took: {}",
+        elapsed_time.as_secs_f32()
+    );
+
+    let distances = calculate_distances(&paths)?;
 
     write_distances_to_csv(DISTANCE_CSV_PATH, &distances)?;
 
+    prioritize_distances(&distances)?;
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{path::PathBuf, str::FromStr};
+
+    use indexmap::IndexMap;
+
+    use crate::prioritize_distances;
+
+    #[test]
+    fn prioritize_distances_valid() {
+        let mut distances: IndexMap<(PathBuf, PathBuf), f32, foldhash::fast::RandomState> =
+            IndexMap::with_hasher(foldhash::fast::RandomState::default());
+
+        distances.insert(
+            (
+                PathBuf::from_str("tc1").unwrap(),
+                PathBuf::from_str("tc2").unwrap(),
+            ),
+            0.57,
+        );
+        distances.insert(
+            (
+                PathBuf::from_str("tc1").unwrap(),
+                PathBuf::from_str("tc3").unwrap(),
+            ),
+            0.78,
+        );
+        distances.insert(
+            (
+                PathBuf::from_str("tc1").unwrap(),
+                PathBuf::from_str("tc4").unwrap(),
+            ),
+            0.8,
+        );
+        distances.insert(
+            (
+                PathBuf::from_str("tc1").unwrap(),
+                PathBuf::from_str("tc5").unwrap(),
+            ),
+            0.78,
+        );
+        distances.insert(
+            (
+                PathBuf::from_str("tc2").unwrap(),
+                PathBuf::from_str("tc3").unwrap(),
+            ),
+            0.75,
+        );
+        distances.insert(
+            (
+                PathBuf::from_str("tc2").unwrap(),
+                PathBuf::from_str("tc4").unwrap(),
+            ),
+            0.77,
+        );
+        distances.insert(
+            (
+                PathBuf::from_str("tc2").unwrap(),
+                PathBuf::from_str("tc5").unwrap(),
+            ),
+            0.75,
+        );
+        distances.insert(
+            (
+                PathBuf::from_str("tc3").unwrap(),
+                PathBuf::from_str("tc4").unwrap(),
+            ),
+            0.37,
+        );
+        distances.insert(
+            (
+                PathBuf::from_str("tc3").unwrap(),
+                PathBuf::from_str("tc5").unwrap(),
+            ),
+            0.69,
+        );
+        distances.insert(
+            (
+                PathBuf::from_str("tc4").unwrap(),
+                PathBuf::from_str("tc5").unwrap(),
+            ),
+            0.8,
+        );
+        let prio = prioritize_distances(&distances).unwrap();
+
+        assert_eq!(
+            prio,
+            ["tc1", "tc4", "tc5", "tc2", "tc3"].map(|x| PathBuf::from_str(x).unwrap())
+        )
+    }
 }
